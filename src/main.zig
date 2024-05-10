165f010cb682326f54bc93b3f2f3818006d840ea
SpandanBG <spandanbg@gmail.com>
2024-05-10 16:43:02 +0530

    update stream in out logic

--- src/main.zig
+++ src/main.zig
@@ -68,8 +68,10 @@ fn get_cli_args(allocator: Allocator) !Config {
 }
 
 fn hex_dump(in: File, out: File, config: Config, allocator: Allocator) void {
-    var line_hex_buf: [10:0]u8 = undefined;
+    var hex_buf: [10:0]u8 = undefined;
+    var should_read_next: bool = true;
     var line_no: u64 = 0;
+    var col_no: u8 = 0;
 
     // Prepare the line to be written to the output file
     var write_str = std.ArrayList(u8).init(allocator);
@@ -79,17 +81,13 @@ fn hex_dump(in: File, out: File, config: Config, allocator: Allocator) void {
     var actual_str = std.ArrayList(u8).init(allocator);
     defer actual_str.deinit();
 
-    var buf: [2:0]u8 = undefined;
-    var should_read_next: bool = true;
-
-    var col_no: u8 = 0;
     while (should_read_next) outer_loop: {
         write_str.clearRetainingCapacity();
         actual_str.clearRetainingCapacity();
         col_no = 0;
 
         (blk: {
-            const line_hex_str = std.fmt.bufPrint(&line_hex_buf, "{x:0>8}: ", .{line_no}) catch |err| break :blk err;
+            const line_hex_str = std.fmt.bufPrint(&hex_buf, "{x:0>8}: ", .{line_no}) catch |err| break :blk err;
             write_str.appendSlice(line_hex_str) catch |err| break :blk err;
             line_no += 0x10;
         }) catch |err| {
@@ -97,31 +95,32 @@ fn hex_dump(in: File, out: File, config: Config, allocator: Allocator) void {
             return;
         };
 
-        while (col_no < config.max_char_per_col) : (col_no += 2) {
-            const r_size = in.read(&buf) catch |err| {
+        for (0..config.max_char_per_col) |i| {
+            const maybe_char = file_next_char(in) catch |err| {
                 std.log.err("error occured while reading from input file = {any}", .{err});
                 return;
             };
-            should_read_next = r_size == buf.len;
-            if (r_size == 0) break;
+            should_read_next = maybe_char != null;
+            const char = maybe_char orelse break;
 
-            const hex_str = dec_to_hex(buf.len, &buf) catch |err| {
+            const hex_str = std.fmt.bufPrint(&hex_buf, "{x:0>2}", .{char}) catch |err| {
                 std.log.err("error occured while creating a hex buffer string = {any}", .{err});
                 return;
             };
 
-            (blk: {
-                write_str.appendSlice(hex_str[0 .. r_size * 2]) catch |err| break :blk err;
-                write_str.append(' ') catch |err| break :blk err;
-            }) catch |err| {
+            write_str.appendSlice(hex_str) catch |err| {
                 std.log.err("error occured while saving to output string = {any}", .{err});
                 return;
             };
 
-            std.mem.replaceScalar(u8, buf[0..], '\n', '.');
-            std.mem.replaceScalar(u8, buf[0..], '\r', '.');
-            std.mem.replaceScalar(u8, buf[0..], '\x00', '.');
-            actual_str.appendSlice(buf[0..r_size]) catch |err| {
+            if (i % 2 != 0) {
+                write_str.append(' ') catch |err| {
+                    std.log.err("error occurd while saving space after hex pair = {any}", .{err});
+                    return;
+                };
+            }
+
+            actual_str.append(if (char == '\n' or char == '\r' or char == '\x00') '.' else char) catch |err| {
                 std.log.err("error occured while saving actual string = {any}", .{err});
                 return;
             };
@@ -156,18 +155,11 @@ fn get_output_writer(out_file: ?([:0]const u8)) File.OpenError!File {
     return std.fs.cwd().createFile(filename, .{});
 }
 
-fn dec_to_hex(comptime n: usize, dec: *const [n:0]u8) std.fmt.BufPrintError![n * 2:0]u8 {
-    var hex: [n * 2:0]u8 = undefined;
-    var buf: [7:0]u8 = undefined;
-
-    for (dec, 0..) |char, i| {
-        const inp: [1:0]u8 = .{char};
-        _ = try std.fmt.bufPrint(&buf, "{x:0>2}", .{inp});
-
-        for (buf[2..4], i * 2..) |hex_char, j| hex[j] = hex_char;
-    }
-
-    return hex;
+fn file_next_char(file: File) !?u8 {
+    var buf: [1:0]u8 = undefined;
+    const r_size = try file.read(&buf);
+    if (r_size == 0) return null;
+    return buf[0];
 }
 
 fn get_max_hex_line_size(col_size: usize) usize {
